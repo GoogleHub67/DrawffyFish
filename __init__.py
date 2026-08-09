d4c93c72d3c9a0e6f6b673830b206bf39243af65
Aarav Patel <aaravpatel14.in@gmail.com>
2026-08-09 16:34:38 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# src/__init__.py
+
+# Expose your main script logic for external imports
+from .main import *
+from .drawffy_brain import *
+from .chat import *
+
+# Package metadata definition
+__version__ = "1.0.0"
+__author__ = "Aarav Patel"
